18232c192c76e5cd578be9f8da01b120aaa8717a
jsmnglng <jgalang@razoyo.com>
2019-08-06 09:04:47 -0500

    Added basic update method

--- index.js
+++ index.js
@@ -5,12 +5,45 @@ const cors = require("cors");
 
 const dummyData = require("./dummyData.json");
 
+app.use(express.json());
 app.use(cors());
 
 app.get("/", (_, res) => res.send("Hello"));
 
 app.get("/dealers", (_, res) => {
   res.status(200).json(dummyData);
+});
+
+app.put("/dealers/:id", (req, res) => {
+  const { id } = req.params;
+  const {
+    name,
+    address,
+    city,
+    phoneNumber,
+    licenseNumber,
+    state,
+    zipCode,
+    enabled,
+    preferred
+  } = req.body;
+  const findDealerById = dealer => dealer.id == id;
+  const foundDealer = dummyData.find(findDealerById);
+
+  if (!foundDealer) {
+    return sendUserError("No Dealer found by that ID", res);
+  } else {
+    if (name) foundDealer.name = name;
+    if (address) foundDealer.address = address;
+    if (city) foundDealer.city = city;
+    if (phoneNumber) foundDealer.phoneNumber = phoneNumber;
+    if (licenseNumber) foundDealer.licenseNumber = licenseNumber;
+    if (state) foundDealer.state = state;
+    if (zipCode) foundDealer.zipCode = zipCode;
+    if (enabled) foundDealer.enabled = enabled;
+    if (preferred) foundDealer.preferred = preferred;
+    res.json(dummyData);
+  }
 });
 
 app.listen(port, () => {
