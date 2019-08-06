36860ec30bf6812bdded4b9bad926de17f30ace5
jsmnglng <jgalang@razoyo.com>
2019-08-06 09:38:17 -0500

    Refactored update method

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
-const dummyData = require("./dummyData.json");
+let dealers = require("./dummyData.json");
 
 app.use(express.json());
 app.use(cors());
@@ -11,38 +11,25 @@ app.use(cors());
 app.get("/", (_, res) => res.send("Hello"));
 
 app.get("/dealers", (_, res) => {
-  res.status(200).json(dummyData);
+  res.status(200).json(dealers);
 });
 
 app.put("/dealers/:id", (req, res) => {
   const { id } = req.params;
-  const {
-    name,
-    address,
-    city,
-    phoneNumber,
-    licenseNumber,
-    state,
-    zipCode,
-    enabled,
-    preferred
-  } = req.body;
-  const findDealerById = dealer => dealer.id == id;
-  const foundDealer = dummyData.find(findDealerById);
+  const dealerIndex = dealers.findIndex(d => d.id == id);
 
-  if (!foundDealer) {
-    return sendUserError("No Dealer found by that ID", res);
+  if (dealerIndex > -1) {
+    const dealer = { ...dealers[dealerIndex], ...req.body };
+
+    dealers = [
+      ...dealers.slice(0, dealerIndex),
+      dealer,
+      ...dealers.slice(dealerIndex + 1)
+    ];
+
+    res.json(dealers);
   } else {
-    if (name) foundDealer.name = name;
-    if (address) foundDealer.address = address;
-    if (city) foundDealer.city = city;
-    if (phoneNumber) foundDealer.phoneNumber = phoneNumber;
-    if (licenseNumber) foundDealer.licenseNumber = licenseNumber;
-    if (state) foundDealer.state = state;
-    if (zipCode) foundDealer.zipCode = zipCode;
-    if (enabled) foundDealer.enabled = enabled;
-    if (preferred) foundDealer.preferred = preferred;
-    res.json(dummyData);
+    res.status(404).json({ msg: "Dealer not found" });
   }
 });
 
