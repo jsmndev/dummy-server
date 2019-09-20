64297fd32569205adcbabbea52abc6e2ef07ea82
jsmnglng <jsmn.glng@gmail.com>
2019-09-20 14:20:04 -0500

    Removed set headers

--- index.js
+++ index.js
@@ -7,26 +7,7 @@ const Json2csvParser = require("json2csv").Parser;
 let dealers = require("./dummyData100.json");
 
 app.use(express.json());
-app.use(cors());
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(cors({origin: '*'}));
 
 app.get("/", (_, res) => res.send("<h1>Mock server for FFL</h1>"));
 
@@ -98,6 +79,7 @@ app.get("/export", (req, res) => {
     res.set("Content-Type", "text/csv");
     res.status(200).end(csvData);
   } 
+
 });
 
 app.post("/import", (req, res) => {
